2b2169fc4c610560b383777a6608414eee752ba0
Elias Caceres <elias.jcy@gmail.com>
2019-07-03 12:12:09 -0400

    [Principal Controller & View]
    - se crea el controlador principal y su vista respectiva para mostrar el 
    listado de estudiantes creados en plataforma
    
    this close #5

---
+++ app/javascript/components/students/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class StudentsIndex extends Component {
+  render() {
+    return <div>Index de Estudiantes</div>;
+  }
+}
+
+export default StudentsIndex;
